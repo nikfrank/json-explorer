2bca2d9a9081f912b2c88387f5dfb772f71ba8bb
nik frank <nikbfrank@gmail.com>
2019-12-16 01:27:48 -0800

    cleanup

--- src/App.js
+++ src/App.js
@@ -19,14 +19,12 @@ function App() {
     }, []);
   
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         { !complexPackage ? (
             <img src={waitingGif} alt='waiting' className='App-waiting' />
         ) : (
-            <>
-              <PackageExplorer pack={complexPackage} />
-            </>
+            <PackageExplorer pack={complexPackage} />
         )}
       </header>
     </div>
